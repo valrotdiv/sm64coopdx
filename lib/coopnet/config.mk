4077ad8cf4f287743275d5fd694158bf3bb81863
vrov <vrov@fakemail.com>
2026-01-05 16:35:33 -0400

    coopnet fix on macos

--- lib/coopnet/config.mk
+++ lib/coopnet/config.mk
@@ -1,9 +1,14 @@
 COOPNET_DIR:=$(BUILD_DIR)/lib/coopnet
+ifeq ($(shell uname -m),arm64)
+OSX_ARM := 1
+else
+OSX_ARM :=
+endif
 
 coopnet: juice
 	@echo "===== Building Coopnet ====="
 	$(call git-clone,https://github.com/coop-deluxe/coopnet.git,$(COOPNET_DIR))
-	@$(MAKE) -C $(COOPNET_DIR)
+	@$(MAKE) -C $(COOPNET_DIR) OSX_BUILD="$(OSX_BUILD)" OSX_ARM="$(OSX_ARM)"
 ifeq ($(MAKE_SHARED_LIBS),1)
 	@cp $(COOPNET_DIR)/bin/libcoopnet.so $(BUILD_DIR)/$(LIB_PREFIX)coopnet$(SH_EXT)
 else
