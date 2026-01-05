3d5b02f61780e58ac44d977a1c84224d6c71278e
vrov <vrov@fakemail.com>
2026-01-05 16:48:11 -0400

    coopnet fix on macos

--- lib/coopnet/config.mk
+++ lib/coopnet/config.mk
@@ -8,7 +8,10 @@ endif
 coopnet: juice
 	@echo "===== Building Coopnet ====="
 	$(call git-clone,https://github.com/coop-deluxe/coopnet.git,$(COOPNET_DIR))
-	@$(MAKE) -C $(COOPNET_DIR) OSX_BUILD="$(OSX_BUILD)" OSX_ARM="$(OSX_ARM)"
+	@$(MAKE) -C $(COOPNET_DIR) \
+	  OSX_BUILD="$(OSX_BUILD)" \
+	  OSX_ARM="$(OSX_ARM)" \
+	  CXXFLAGS="-Wall -Werror -Wno-unused-function -Wdeprecated-declarations -std=c++11 -fPIC -DJUICE_STATIC -g"
 ifeq ($(MAKE_SHARED_LIBS),1)
 	@cp $(COOPNET_DIR)/bin/libcoopnet.so $(BUILD_DIR)/$(LIB_PREFIX)coopnet$(SH_EXT)
 else
